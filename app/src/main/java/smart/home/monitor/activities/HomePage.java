47923aed10c56f9485e184c7c9c290268ce7b291
Deepak <sngd0512@humbermail.ca>
2020-11-18 16:33:10 -0500

    temp sensor

--- app/src/main/java/smart/home/monitor/activities/HomePage.java
+++ app/src/main/java/smart/home/monitor/activities/HomePage.java
@@ -3,13 +3,32 @@ package smart.home.monitor.activities;
 import androidx.appcompat.app.AppCompatActivity;
 import smart.home.monitor.R;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HomePage extends AppCompatActivity {
+private SensorManager mgr;
+private Sensor temp;
+private TextView text;
+private StringBuilder msg = new StringBuilder(2048);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+         mgr = (SensorManager) this.getSystemService(SENSOR_SERVICE);
+         temp = mgr.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+         text = (TextView) findViewById(R.id.text);
     }
+    @Override
+    protected void onResume() {
+     mgr.registerListener(this, temp, SensorManager.SENSOR_DELAY_NORMAL);
+     super.onResume();
+    }
+@Override
+protected void onPause() {
+        mgr.unregisterListener(this, temp);
+        super.onPause();
 }
